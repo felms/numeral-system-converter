30b925a5c2b9d8359c90f1515096b0fe8c645d8c
Felipe <felipemoreira1@gmail.com>
2020-06-10 12:05:17 -0300

    Project: Numeral System Converter → Stage 2/6: Almost an octopus

--- converter/Main.java
+++ converter/Main.java
@@ -1,9 +1,15 @@
 package converter;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        int number = 42;
-        System.out.printf("%d is equal to %s\n", number, decimalToBinary(number));
+        Scanner scanner = new Scanner(System.in);
+        
+        int number = scanner.nextInt();
+        String base8 = decimalToBase8(number);
+        
+        System.out.println(base8.charAt(base8.length() - 1));
     }
     
     public static String decimalToBinary(int decimalNumber) {
@@ -14,6 +20,18 @@ public class Main {
             decimalNumber /= 2;
         }
         
-        return "0b"+ binNumber.reverse().toString();
+        return "0b"+ (binNumber.length() > 0 ? binNumber.reverse().toString() : "0");
+    }    
+    
+    public static String decimalToBase8(int decimalNumber) {
+        StringBuilder base8Number = new StringBuilder();
+        
+        while (decimalNumber > 0) {
+            base8Number.append(decimalNumber % 8);
+            decimalNumber /= 8;
+        }
+        
+        return base8Number.length() > 0 ? base8Number.reverse().toString() : "0";
     }
 }
+
