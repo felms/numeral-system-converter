c3c784c3eaaf35f674160bb8dd19730a33d8a54b
Felipe <felipemoreira1@gmail.com>
2020-06-13 21:36:44 -0300

    Project: Numeral System Converter → Stage 4/6: Any which radix

--- converter/Main.java
+++ converter/Main.java
@@ -6,62 +6,33 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         
-        int number = scanner.nextInt();
-        int base  = scanner.nextInt();
+        int sourceRadix = Integer.parseInt(scanner.nextLine());
+        String sourceNumber  = scanner.nextLine();
+        int targetRadix = Integer.parseInt(scanner.nextLine());
+
+        int decNumber;
+
+        if (sourceRadix == 1) {
+            decNumber = sourceNumber.length();
+        } else {
+            decNumber = Integer.parseInt(sourceNumber, sourceRadix);
+        }
+
         String answer = "";
 
-        switch(base) {
-            case 2:
-                answer = decimalToBinary(number);
-                break;
-            
-            case 8:
-                answer = decimalToOctal(number);
-                break;
+        if (targetRadix == 1) {
+            StringBuilder n = new StringBuilder();
+            for (int i = 0; i < decNumber; i++) {
+                n.append("1");
+            }
 
-            case 16:
-                answer = decimalToHexadecimal(number);
-                break;
-
+            answer = n.toString();
+        } else {
+            answer = Integer.toString(decNumber, targetRadix);
         }
         
         System.out.println(answer);
     }
     
-    public static String decimalToBinary(int decimalNumber) {
-        StringBuilder binNumber = new StringBuilder();
-        
-        while (decimalNumber > 0) {
-            binNumber.append(decimalNumber % 2);
-            decimalNumber /= 2;
-        }
-        
-        return "0b" + (binNumber.length() > 0 ? binNumber.reverse().toString() : "0");
-    }    
-    
-    public static String decimalToOctal(int decimalNumber) {
-        StringBuilder octalNumber = new StringBuilder();
-        
-        while (decimalNumber > 0) {
-            octalNumber.append(decimalNumber % 8);
-            decimalNumber /= 8;
-        }
-        
-        return "0" + (octalNumber.length() > 0 ? octalNumber.reverse().toString() : "0");
-    }
-
-    public static String decimalToHexadecimal(int decimalNumber) {
-        StringBuilder hexadecimalNumber = new StringBuilder();
-        String hexValues = "0123456789abcdef";
-        
-        while (decimalNumber > 0) {
-            int digit = decimalNumber % 16;
-            char hexDigit = hexValues.charAt(digit);
-            hexadecimalNumber.append(hexDigit);
-            decimalNumber /= 16;
-        }
-        
-        return "0x" + (hexadecimalNumber.length() > 0 ? hexadecimalNumber.reverse().toString() : "0");
-    }
 }
 
