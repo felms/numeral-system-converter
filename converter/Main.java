de401ae9a14a87bf66d97742aa1d9dce6969f2eb
Felipe <felipemoreira1@gmail.com>
2020-06-13 21:12:50 -0300

    Project: Numeral System Converter → Stage 3/6: Convert decimals

--- converter/Main.java
+++ converter/Main.java
@@ -7,9 +7,25 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         
         int number = scanner.nextInt();
-        String base8 = decimalToBase8(number);
+        int base  = scanner.nextInt();
+        String answer = "";
+
+        switch(base) {
+            case 2:
+                answer = decimalToBinary(number);
+                break;
+            
+            case 8:
+                answer = decimalToOctal(number);
+                break;
+
+            case 16:
+                answer = decimalToHexadecimal(number);
+                break;
+
+        }
         
-        System.out.println(base8.charAt(base8.length() - 1));
+        System.out.println(answer);
     }
     
     public static String decimalToBinary(int decimalNumber) {
@@ -20,18 +36,32 @@ public class Main {
             decimalNumber /= 2;
         }
         
-        return "0b"+ (binNumber.length() > 0 ? binNumber.reverse().toString() : "0");
+        return "0b" + (binNumber.length() > 0 ? binNumber.reverse().toString() : "0");
     }    
     
-    public static String decimalToBase8(int decimalNumber) {
-        StringBuilder base8Number = new StringBuilder();
+    public static String decimalToOctal(int decimalNumber) {
+        StringBuilder octalNumber = new StringBuilder();
         
         while (decimalNumber > 0) {
-            base8Number.append(decimalNumber % 8);
+            octalNumber.append(decimalNumber % 8);
             decimalNumber /= 8;
         }
         
-        return base8Number.length() > 0 ? base8Number.reverse().toString() : "0";
+        return "0" + (octalNumber.length() > 0 ? octalNumber.reverse().toString() : "0");
+    }
+
+    public static String decimalToHexadecimal(int decimalNumber) {
+        StringBuilder hexadecimalNumber = new StringBuilder();
+        String hexValues = "0123456789abcdef";
+        
+        while (decimalNumber > 0) {
+            int digit = decimalNumber % 16;
+            char hexDigit = hexValues.charAt(digit);
+            hexadecimalNumber.append(hexDigit);
+            decimalNumber /= 16;
+        }
+        
+        return "0x" + (hexadecimalNumber.length() > 0 ? hexadecimalNumber.reverse().toString() : "0");
     }
 }
 
