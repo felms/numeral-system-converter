8c170924471db06e84d827b8c4077b3b3c8f98e7
Felipe <felipemoreira1@gmail.com>
2020-06-16 20:45:40 -0300

    Project: Numeral System Converter → Stage 5/6: Converting fractions

--- converter/Main.java
+++ converter/Main.java
@@ -10,6 +10,15 @@ public class Main {
         String sourceNumber  = scanner.nextLine();
         int targetRadix = Integer.parseInt(scanner.nextLine());
 
+        String number = sourceNumber.contains(".") 
+                        ? fractionalConverter(sourceRadix, sourceNumber, targetRadix)
+                        : integerConverter(sourceRadix, sourceNumber, targetRadix) ;
+        
+        System.out.println(number);
+    }
+
+    public static String integerConverter(int sourceRadix, String sourceNumber, int targetRadix) {
+
         int decNumber;
 
         if (sourceRadix == 1) {
@@ -31,7 +40,74 @@ public class Main {
             answer = Integer.toString(decNumber, targetRadix);
         }
         
-        System.out.println(answer);
+        return answer;
+
+    }
+
+    public static String fractionalConverter(int sourceRadix, String sourceNumber, int targetRadix) {
+
+        String result = "";
+        
+        if (sourceRadix == 10) {
+            result = fracBaseTenToBaseN(sourceNumber, targetRadix);
+        } else if (targetRadix == 10) {
+            result = fracBaseNToBaseTen(sourceRadix, sourceNumber);
+        } else {
+            String r = fracBaseNToBaseTen(sourceRadix, sourceNumber);
+            result = fracBaseTenToBaseN(r, targetRadix);
+        }
+
+        return result;
+    }
+
+    public static String fracBaseNToBaseTen(int sourceRadix, String sourceNumber) {
+
+        String[] parts = sourceNumber.split("\\.");
+
+        // Convert the integer part
+        double integerPart = Integer.parseInt(parts[0], sourceRadix);
+
+        // Convert the fractional part
+        double decimalValue = 0.0;
+        for (int i = 0; i < parts[1].length(); i++) {
+            double parsedNumber = (double)Integer.parseInt(Character.toString(parts[1].charAt(i)), sourceRadix);
+
+            decimalValue += parsedNumber / Math.pow(sourceRadix, i + 1);
+        }
+
+        return String.valueOf(integerPart + decimalValue);
+    }
+
+    public static String fracBaseTenToBaseN(String sourceNumber, int targetRadix) {
+        
+        String[] parts = sourceNumber.split("\\.");
+
+        // Convert the integer part
+        int decNumber = Integer.parseInt(parts[0]);
+        String integerPart = Integer.toString(decNumber, targetRadix);
+        
+        if (targetRadix == 1) {
+            StringBuilder n = new StringBuilder();
+            for (int i = 0; i < decNumber; i++) {
+                n.append("1");
+            }
+            return n.toString();
+        }
+
+        // Convert the fractional part
+        String decimalValue = "";
+        int digits = 5;
+        double number = Double.parseDouble("0." + parts[1]);
+
+        for (int i = 0; i < digits; i++) {
+            double d = number * targetRadix;
+            String doubleAsText = String.valueOf(d);
+            int decPart = Integer.parseInt(doubleAsText.split("\\.")[0]);
+            decimalValue += Integer.toString(decPart, targetRadix);
+            number = d - decPart;
+        }
+
+        return integerPart + "." + decimalValue;
     }
     
 }
